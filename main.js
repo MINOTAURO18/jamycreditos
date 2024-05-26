377cfee555d0237b46652507621f20b19ed60efe
alexander <alexander12345my@gmail.com>
2024-05-26 18:31:51 -0500

    section five almost done

--- main.js
+++ main.js
@@ -53,3 +53,42 @@ function getBotResponse(input) {
     const normalizedInput = input.toLowerCase().trim();
     return responses[normalizedInput] || 'Para que tengamos una conversación más fluida, habla conmigo en <a href="https://web.whatsapp.com/" target="_blank">WhatsApp</a>!';
 }
+
+
+// caculador 
+
+function calcularPrestamo() {
+    const monto = parseFloat(document.getElementById('monto').value);
+    const plazo15 = document.getElementById('plazo15').checked;
+    const plazo30 = document.getElementById('plazo30').checked;
+    
+    let resultado = document.getElementById('resultado');
+    
+    if (isNaN(monto) || monto < 50000 || monto > 150000) {
+        resultado.textContent = 'Por favor, ingrese un monto válido entre 50,000 y 150,000 pesos.';
+        return;
+    }
+    
+    let interes = 0;
+    
+    if (plazo15) {
+        interes = monto * .2;
+
+    } else if (plazo30 && monto > 99999) {
+        interes = monto * 0.25;
+    } else if (plazo30){
+        interes = monto * 0.4;
+    }
+    
+    else {
+        resultado.textContent = 'Por favor, seleccione un plazo.';
+        return;
+    }
+    
+    const totalPagar = monto + interes;
+    resultado.innerHTML = `
+        <p>Intereses: ${interes.toLocaleString('es-MX')} pesos</p>
+        <p>Total a pagar: ${totalPagar.toLocaleString('es-MX')} pesos</p>
+    `;
+}
+
